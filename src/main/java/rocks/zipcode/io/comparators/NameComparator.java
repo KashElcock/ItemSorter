ec3b7f4a6e0d5cc87fc2603ca9856ab0885e9eb2
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-12-07 12:01:14 -0500

    ID and Name Comparator tests passed

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,19 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item1, Item item2) {
+//        if(Objects.equals(item1.getName(), item2.getName())) return 0;
+//        if(item1.getName()== null) return -1;
+//        if(item2.getName() == null) return 1;
+        return item1.getName().compareTo(item2.getName());
+    }
 }
